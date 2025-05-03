c78708db9aa3df5facfaea3c0e0ff9450514b190
Clément Léonard <leonard.clement@unifr.ch>
2025-05-03 11:43:01 +0200

    Renamed tables and made occupancy and room_info.

--- server/models.py
+++ server/models.py
@@ -3,24 +3,54 @@ from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
 from sqlalchemy import func
 
-# Table representing the data collected by the atom-lites
-class BLEDevices(db.Model):
-    __tablename__ = 'bledevices' # name of he database table
-    
+# Table representing the data recorded by the atom-lites
+class Recordings(db.Model):    
     id: Mapped[int] = mapped_column(primary_key=True)
     time: Mapped[datetime] = mapped_column(server_default=func.current_timestamp())
     room: Mapped[str]
     count: Mapped[int]
 
     def __repr__(self):
-        return f'id:{self.id}, time: {self.time}, room: {self.room}, count: {self.count}'
+        return f"[{self.id}][{self.time}] Room '{self.room}' contains {self.count} bluetooth devices."
     
+# contains static information about the room
+class RoomInfo(db.Model):
+    room: Mapped[str] = mapped_column(primary_key=True)
+    capacity: Mapped[int] # number of places in the room when empty
+
+    def __repr__(self):
+        return f"Room '{self.room}' has a capacity of {self.capacity} people."
+
 # Table representing historical room occupancy
-# class Occupancy(db.Model):
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     time: Mapped[datetime] = mapped_column(server_default=func.current_timestamp())
-#     occupancy: Mapped[float]
+class Occupancy(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    time: Mapped[datetime]
+    room: Mapped[str]
+    occupancy: Mapped[float]
+
+    def __repr__(self):
+        return f"[{self.id}][{self.time}] Room '{self.room}' is occupied to {self.occupancy:.0%}."
+
 
 # NOTE: every time this scheme is updated, you have to run
 #   flask db migrate
 #   flask db upgrade
+
+# NOTE: you can fill some random data with the following command:
+
+# # Create some rooms
+# INSERT INTO room_info (room, capacity) VALUES
+# ('infolab 0', 20),
+# ('infolab 1', 20),
+# ('library', 100); # ...
+
+# # Create some occupancy information
+# INSERT INTO recordings (room, count) VALUES
+# ('infolab 0', 22),
+# ('infolab 1', 18); # ...
+
+# # Calculate the occupancy in the databse
+# INSERT INTO occupancy (time, room, occupancy) 
+# SELECT r.time, r.room, CAST(r.count AS REAL) / ri.capacity
+# FROM recordings r
+# JOIN room_info ri ON r.room == ri.room
--- server/routes.py
+++ server/routes.py
@@ -1,31 +1,46 @@
 from flask import render_template, request, jsonify
-from models import BLEDevices
+from models import Recordings, Occupancy, RoomInfo
 from markupsafe import escape
 
 def register_routes(app, db):
     @app.route('/')
     def index():
-        devices = BLEDevices.query.all()
+        devices = Recordings.query.all()
         print(f"Something connected: {request.headers}")
         return render_template('index.html', queries=devices)
     
     # curl --request POST http://127.0.0.1:5000/collect?room=INFOLAB0&count=7
-    @app.route('/collect', methods=['POST'])
+    @app.route('/collect', methods=['POST', 'GET'])
     def collect():
-        data = request.get_json()
-        room = data.get('room')
-        count = data.get('count')
-        if room and isinstance(count, int):
-            device = BLEDevices(room=room, count=count)
-            db.session.add(device)
-            db.session.commit()
-            return jsonify({'message': 'Data received and stored successfully'}), 201
-        else:
-            return jsonify({'error': 'Invalid JSON data.'}), 400
+        room: str = ''
+        count: int = 0
+
+        if request.method == 'POST':
+            data = request.get_json()
+            room = data.get('room')
+            count = data.get('count')
+            if room and isinstance(count, int):
+                device = Recordings(room=room, count=count)
+                db.session.add(device)
+                db.session.commit()
+                return jsonify({'message': 'Data received and stored successfully'}), 201
+            else:
+                return jsonify({'error': 'Invalid JSON data.'}), 400
+        elif request.method == 'GET':
+            data = request.args
+            room = data['room']
+            count = int(data['count'])
+            # check for errors
+        # calculate occupancy, commit it to database
+
         
     @app.route('/room/<room>')
     def room(room):
         room = escape(room)
-        query = db.select(BLEDevices).where(BLEDevices.room == room)
-        result = db.session.execute(query)
-        return render_template('room.html', queries=result, room=room)
+        query = db.select(Recordings).where(Recordings.room == room)
+        recordings = db.session.execute(query)
+
+        query = db.select(Occupancy).where(Occupancy.room == room)
+        occupancies = db.session.execute(query)
+        
+        return render_template('room.html', queries=recordings, room=room, occupancies=occupancies)
